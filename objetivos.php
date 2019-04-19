550ad8173581552ea460f031b061ac58f4a4c049
Maurício <mauriciogspagnol@gmail.com>
2019-04-19 15:40:13 -0300

    objetivos em andamento

---
+++ objetivos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>RHX</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+	<?php 
+	include("menu.php");
+	include("funcoes.php")
+	?>
+
+	
+	
+	<?php 
+	include("footer.php");
+	?>
+</body>
+</html>
