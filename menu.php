4865b02cbb65a0077c8e4629e36d034fb3ac4c3e
Maurício <mauriciogspagnol@gmail.com>
2019-05-14 04:43:26 -0300

    ref noticia pronto

--- menu.php
+++ menu.php
@@ -10,10 +10,11 @@
   <div class="col-sm-4" id="bloco_menu">
   	 <ul style="list-style: none; margin-top: 30px;">
 		<b>
-		<li class="col-sm-3 col-xs-2">Home</li>
-		<li class="col-sm-3 col-xs-4">Informativo</li>
-		<li class="col-sm-3 col-xs-3" id="item_menu">Objetivos</li>
-		<li class="col-sm-3 col-xs-3" id="item_menu2">Fórum</li>
+		<li class="col-sm-2 col-xs-2"><a href="index.php" style="text-decoration: none;color: #333333;">Home</a></li>
+		<li class="col-sm-3 col-xs-4"><a href="informativo.php" style="text-decoration: none;color: #333333;">Informativo</a></li>
+		<li class="col-sm-3 col-xs-3" id="item_menu"><a href="objetivos.php" style="text-decoration: none;color: #333333;">Objetivos</a></li>
+		<li class="col-sm-3 col-xs-3" id="item_menu2"><a href="forum/painel.php" style="text-decoration: none;color: #333333;">Fórum
+		</a></li>
 		</b>
 	</ul>
 
--- visualizaNoticia.php
+++ visualizaNoticia.php
@@ -1,61 +1,21 @@
-<!DOCTYPE>
+<?php 
+	if(isset($_GET['id'])){
+		$id = "$_GET[id]";
+	}
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
-	<title>RHX</title>
+	<title></title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
 	<?php 
 	include("menu.php");
-	include("funcoes.php")
+	include("funcoes.php");
 	?>
-
-	<!-- Abas nav -->
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item col-sm-3">
-    <a class="nav-link active" id="oQueE-tab" data-toggle="tab" role="tab" aria-controls="oQueE" aria-selected="true"
-    style="background: green;border-radius: 100px 100px 0 0;color: #eee;text-align: center;" onclick="alteraConteudo('oQueE')" 
-    >O que é?</a>
-  </li>
-  <li class="nav-item col-sm-3">
-    <a class="nav-link" id="tratamento-tab" data-toggle="tab"  role="tab" aria-controls="tratamento" aria-selected="false"
-   style="background: red;border-radius: 100px 100px 0 0;color: #eee;text-align: center;" onclick="alteraConteudo('tratamento')">Tratamento</a>
-  </li>
-  <li class="nav-item col-sm-3">
-    <a class="nav-link" id="objetivos-tab" data-toggle="tab" role="tab" aria-controls="objetivos" aria-selected="false"
-    style="background: black;border-radius: 100px 100px 0 0;color: #eee;text-align: center;" onclick="alteraConteudo('objetivos')" 
-    >Objetivos</a>
-  </li>
- 
-</ul>
-
-<!-- Painel de abas -->
-<div class="tab-content">
-  <div class="tab-pane active" id="oQueE" role="tabpanel" aria-labelledby="oQueE-tab" style="background: green;color: #eee">O que é?</div>
-  <div class="tab-pane" id="tratamento" role="tabpanel" aria-labelledby="tratamento-tab" style="background: red;color: #eee">Tratamento</div>
-  <div class="tab-pane" id="objetivos" role="tabpanel" aria-labelledby="objetivos-tab" style="background: black;color: #eee">Objetivos</div>
-</div>
-
-<script type="text/javascript">
-	function alteraConteudo(menu){
-		if(menu == "oQueE"){
-			document.getElementById('oQueE').setAttribute("class","tab-pane active");
-			document.getElementById('tratamento').setAttribute("class","tab-pane");
-			document.getElementById('objetivos').setAttribute("class","tab-pane");
-		} else if (menu == "tratamento"){
-			document.getElementById('oQueE').setAttribute("class","tab-pane");
-			document.getElementById('tratamento').setAttribute("class","tab-pane active");
-			document.getElementById('objetivos').setAttribute("class","tab-pane");
-		} else {
-			document.getElementById('oQueE').setAttribute("class","tab-pane");
-			document.getElementById('tratamento').setAttribute("class","tab-pane");
-			document.getElementById('objetivos').setAttribute("class","tab-pane active");
-		}
-	}
-	
-</script>
-
 
 	<div class="col-sm-8">
 		<h2 style="text-align: center;font-family: Bookman;">Informativo</h2>
@@ -63,8 +23,7 @@
 		<div class=" panel-body">
         <ul class="list-group">
     	<?php 
-            $resultado = lista_noticias();
-    		foreach ($resultado as $result) {?>
+            $result = busca_noticia($id);?>
     				
     			<div class="col-sm-12 col-xs-12" style="margin-top: 10px;">	
     				<img src="<?=$result['imagem']?>" class="col-sm-2 col-xs-5" style="height: 100px; border: solid 1px #000; padding: 0;">
@@ -78,12 +37,10 @@
 					</li>
 					<li style=" list-style-type:none" class="col-lg-8 col-lg-push-1 col-xs-6">
 						<?php
-							echo substr($result['descricao'], 0, 300);
-						?>	... <a href="">ver mais</a>
+							echo $result['descricao']
+						?>
 					</li>
 				</div>
-			
-		<?php } ?>
         </ul>
     </div>
 	</div>
@@ -119,7 +76,7 @@
 					<div style="margin-left: 20px;">
 			<img class="col-sm-1" src="http://www.fazenda.gov.br/orgaos/colegiados/crsfn/imagens/email/@@images/59b4210b-2170-4aaa-8a77-988c83dbf1b0.png" style="padding: 0;margin-top: 5px;">
 			</div>
-				<b class="col-sm-8" style="font-size: 20px;">atendimento@rhx.org</b>
+				<b class="col-sm-8" style="font-size: 20px;"> atendimento@rhx.org</b>
 			</a>
 			</td>
 			</tr>
@@ -133,7 +90,7 @@
 					<div style="margin-left: 20px;">
 			<img class="col-sm-1" src="https://1.bp.blogspot.com/-85-K3datVr8/WMATuRxupBI/AAAAAAAAl1E/UZc4SEeD_JwUbS5KmD6JzpwmOGFBfIojgCLcB/s1600/Daniel%2BStudios%2BPack%2BRender%2B%252820%2529.png" style="padding: 0;margin-top: 5px;">
 			</div>
-				<b class="col-sm-8" style="font-size: 20px;"><a href="forum/painel.php" style="text-decoration: none;color: #333333;">Fórum</a></b>
+				<b class="col-sm-8" style="font-size: 20px;">Fórum</b>
 				</a>
 			</a>
 			</td>
@@ -141,8 +98,7 @@
 		</table>
 		
 	</div>
-
-		<?php 
+<?php 
 	include("footer.php");
 	?>
 </body>
